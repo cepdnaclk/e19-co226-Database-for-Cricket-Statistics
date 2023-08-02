20c159cdc42a4565fdbf6b3c84fde48bd9f193dc
kaushithamsilva <kaushithamsilva@gmail.com>
2023-08-02 14:20:03 +0530

    AddedLogo

--- frontEnd/criclive/src/App.js
+++ frontEnd/criclive/src/App.js
@@ -3,6 +3,11 @@ import styles from "./styles/App.module.scss";
 const App = () => {
   return (
     <div className={styles.container}>
+      <img
+        className={styles.logo}
+        src="./CricLive-Dark.svg"
+        alt="cricLive Logo"
+      />
       <MatchHeader />
     </div>
   );
