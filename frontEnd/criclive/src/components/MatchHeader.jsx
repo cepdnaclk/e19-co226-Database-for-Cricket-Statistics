595cceeb9cf735e43419fd9f8ab5f4ffcf6384ef
kaushithamsilva <kaushithamsilva@gmail.com>
2023-08-02 14:14:57 +0530

    Header Completed

--- frontEnd/criclive/src/components/MatchHeader.jsx
+++ frontEnd/criclive/src/components/MatchHeader.jsx
@@ -1,12 +1,36 @@
 import React from "react";
 import styles from "../styles/MatchHeader.module.scss";
-
+import Flag from "react-flagkit";
+const FLAG_SIZE = 65;
 function MatchHeader() {
   return (
     <div className={styles.header}>
       <div className={styles.scores}>
-        <div className={styles.home}></div>
-        <div className={styles.away}></div>
+        <div className={styles.home}>
+          <div className={styles.flagAndName}>
+            <Flag country="LK" size={FLAG_SIZE} />
+            <p>Sri Lanka</p>
+          </div>
+          <div className={styles.score}>
+            <p className={styles.runsWickets}>225</p>
+            <p className={styles.overs}>(49.5)</p>
+          </div>
+        </div>
+        <div className={styles.away}>
+          <div className={styles.flagAndName}>
+            <Flag country="IN" size={FLAG_SIZE} />
+            <p>India</p>
+          </div>
+          <div className={styles.score}>
+            <p className={styles.runsWickets}>130/5</p>
+            <p className={styles.overs}>(23)</p>
+          </div>
+        </div>
+      </div>
+
+      <div className={styles.status}>
+        <p>IND needs 96 runs in 27 Overs</p>
+        <p className={styles.matchInfo}>Final · Asia Cup 2023</p>
       </div>
     </div>
   );
