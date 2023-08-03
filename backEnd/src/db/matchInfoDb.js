59668e1db3913d9f6309c029a7625452ccf7a770
Kanishka Gunawardana <kanishkagunawarthana@gmail.com>
2023-08-03 08:51:45 +0530

    teamInfo api get

--- backEnd/src/db/matchInfoDb.js
+++ backEnd/src/db/matchInfoDb.js
@@ -1,19 +1,25 @@
 const db = require("../../dbConfig/dbConfig")
 
 const sql_matchDetails = "SELECT * FROM CURRENTMATCH";
-const sql_teamDetails = "SELECT * FROM PLAYER INNER JOIN TEAMPLAYERS ON PLAYER.PlayerID = TEAMPLAYERS.PlayerID WHERE TeamID = ";
+const sql_teamDetails = "SELECT TeamID, TeamName, Country, Coach, CaptainID FROM CURRENTMATCH INNER JOIN TEAM ON TEAM.TeamID = CURRENTMATCH.Team1_ID UNION SELECT TeamID, TeamName, Country, Coach, CaptainID FROM CURRENTMATCH INNER JOIN TEAM ON TEAM.TeamID = CURRENTMATCH.Team2_ID";
+const sql_playerDetails = "SELECT * FROM PLAYER INNER JOIN TEAMPLAYERS ON PLAYER.PlayerID = TEAMPLAYERS.PlayerID WHERE TeamID = ";
 const sql_teamCaptain = "SELECT CaptainID FROM TEAM WHERE TeamID = ";
 
 function getMatchInfo(res){
     db.query(sql_matchDetails,(err, data) => {
-        console.log(err);
+        res.status(200).json(data[0]);
+    });
+}
+
+function getTeamInfo(res){
+    db.query(sql_teamDetails,(err, data) => {
         res.status(200).json(data);
     });
 }
 
-function getTeamInfo(teamId, res){
+function getPlayerInfo(teamId, res){
 
-    db.query(sql_teamDetails + teamId,(err, players) => {
+    db.query(sql_playerDetails + teamId,(err, players) => {
         db.query(sql_teamCaptain + teamId,(err, captainPlayer) => {
             data = players.map(player => (player.PlayerID === captainPlayer[0].CaptainID) ? {captain:true, ...player}:{captain:false, ...player});
             res.status(200).json(data);
@@ -21,4 +27,4 @@ function getTeamInfo(teamId, res){
     });
 }
 
-module.exports = {getMatchInfo, getTeamInfo};
+module.exports = {getMatchInfo, getTeamInfo, getPlayerInfo};
