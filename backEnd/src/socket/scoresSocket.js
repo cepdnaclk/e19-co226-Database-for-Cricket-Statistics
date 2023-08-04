b61254c19481a2a98eb89d1d87f9a1082bfcfc61
Kanishka Gunawardana <kanishkagunawarthana@gmail.com>
2023-08-04 13:21:40 +0530

    scores wicket and over backEnd

--- backEnd/src/socket/scoresSocket.js
+++ backEnd/src/socket/scoresSocket.js
@@ -1,12 +1,15 @@
 const topics = require("../util/topics");
 
-function battingScoreSend(io, data){
-    io.emit(topics.BATTING, data);
+function battingScoreSend(io, data1, data2){
+    io.emit(topics.ONEBATTING, data1);
+    io.emit(topics.TWOBATTING, data2);
 }
 
-function ballingScoreSend(io, data){
-    io.emit(topics.BALLING, data);
+function ballingScoreSend(io, data1, data2){
+    io.emit(topics.ONEBALLING, data1);
+    io.emit(topics.TWOBALLING, data2);
 }
+
 
 module.exports = {battingScoreSend, ballingScoreSend};
 
