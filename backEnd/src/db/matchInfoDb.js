f1d9702e3c621c147f997be58fabb10380ed07ad
Kanishka Gunawardana <kanishkagunawarthana@gmail.com>
2023-08-06 09:24:51 +0530

    changing /teams/details GET

--- backEnd/src/db/matchInfoDb.js
+++ backEnd/src/db/matchInfoDb.js
@@ -16,12 +16,12 @@ const sql_teamCaptain = "SELECT CaptainID FROM TEAM WHERE TeamID = ";
 function getMatchInfo(res){
     db.query(sql_matchDetails,(err, data) => {
         res.status(200).json({
-            team1Id:data[0].Team1_ID,
-            team2Id:data[0].Team1_ID,
+            // team1Id:data[0].Team1_ID,
+            // team2Id:data[0].Team1_ID,
             date:data[0].Date,
             time:data[0].Time,
             venue:data[0].Venue,
-            toss:data[0].Toss,
+            toss:(data[0].Toss===1)?true:false,
             matchName:data[0].MatchName,
             tossIsBatting:data[0].TossIsBatting
         });
