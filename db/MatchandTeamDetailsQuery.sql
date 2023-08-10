adf4c67316b2784346f75c8d6c2be555a6b13ff6
Sanduni Ubayasiri <115542118+SanduniU@users.noreply.github.com>
2023-08-10 19:10:01 +0530

    issue corrected in maiden overs

--- db/MatchandTeamDetailsQuery.sql
+++ db/MatchandTeamDetailsQuery.sql
@@ -1,6 +1,6 @@
---get match details
+-- get match details
 
---team names
+-- team names
 select TeamName as Team1 from TEAM
 Where TeamID = (Select Team1_ID from CURRENTMATCH);
 
@@ -13,16 +13,16 @@ Where TeamID = (Select Team2_ID from CURRENTMATCH);
 select * from TEAM
 Where TeamID = (Select Team2_ID from CURRENTMATCH);
 
---other details
+-- other details
 Select Date, time, venue, MatchName from CURRENTMATCH;
 
---toss
+-- toss
 Select TeamName as TossWon from TEAM
 WHERE TeamID =(SELECT Toss from CURRENTMATCH);
 
 SELECT TossIsBatting FROM CURRENTMATCH;
 
---view to show match details
+-- view to show match details
 CREATE VIEW MatchDetails AS
 SELECT
     (SELECT TeamName FROM TEAM WHERE TeamID = (SELECT Team1_ID FROM CURRENTMATCH)) AS Team1,
@@ -37,22 +37,22 @@ FROM CURRENTMATCH AS CM;
 
 SELECT * FROM MatchDetails;
 
---get team details
---team 1
+-- get team details
+-- team 1
 SELECT PlayerName FROM PLAYER
 INNER JOIN TEAMPLAYERS ON PLAYER.PlayerID = TEAMPLAYERS.PlayerID
 WHERE TEAMPLAYERS.TeamID = (SELECT Team1_ID FROM CURRENTMATCH);
---captain
+-- captain
 SELECT PlayerName as Captain from PLAYER
 WHERE PlayerID = (  SELECT CaptainID from TEAM 
                     WHERE TeamID = (SELECT Team1_ID FROM CURRENTMATCH));
 
---team 2
+-- team 2
 SELECT PlayerName FROM PLAYER
 INNER JOIN TEAMPLAYERS ON PLAYER.PlayerID = TEAMPLAYERS.PlayerID
 WHERE TEAMPLAYERS.TeamID = (SELECT Team2_ID FROM CURRENTMATCH);
 
---captain
+-- captain
 SELECT PlayerName as Captain from PLAYER
 WHERE PlayerID = (  SELECT CaptainID from TEAM 
                     WHERE TeamID = (SELECT Team2_ID FROM CURRENTMATCH));
