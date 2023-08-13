faa4f4855bbf3aab01f4501561cf51424670dc72
Kanishka Gunawardana <kanishkagunawarthana@gmail.com>
2023-08-13 12:16:36 +0530

    save

--- backEnd/dbConfig/dbConfig.js
+++ backEnd/dbConfig/dbConfig.js
@@ -11,17 +11,17 @@ const mysql = require("mysql");
 // });
 
 
-const dbConn = mysql.createConnection({
-  host: 'gateway01.us-west-2.prod.aws.tidbcloud.com',
-  port: 4000,
-  user: '3m7pRf38p5JMiTT.root',
-  password: 'eVl1nT9ZuSaRReP8',
-  database: 'test',
-  ssl: {
-    minVersion: 'TLSv1.2',
-    rejectUnauthorized: true
-  }
-});
+// const dbConn = mysql.createConnection({
+//   host: 'gateway01.us-west-2.prod.aws.tidbcloud.com',
+//   port: 4000,
+//   user: '3m7pRf38p5JMiTT.root',
+//   password: 'eVl1nT9ZuSaRReP8',
+//   database: 'test',
+//   ssl: {
+//     minVersion: 'TLSv1.2',
+//     rejectUnauthorized: true
+//   }
+// });
 
 
 const dbConn = mysql.createConnection({
