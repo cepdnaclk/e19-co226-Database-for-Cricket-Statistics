cbba9209c574db24806567122e97202a2ca127a4
kaushithamsilva <kaushithamsilva@gmail.com>
2023-08-15 14:36:50 +0530

    PWA Launch

--- frontEnd/criclive/src/services/api.js
+++ frontEnd/criclive/src/services/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "http://10.30.7.70:5000",
 });
--- frontEnd/criclive/src/services/socket.js
+++ frontEnd/criclive/src/services/socket.js
@@ -1,3 +1,3 @@
 import { io } from "socket.io-client";
-const socket = io("http://localhost:5000");
+const socket = io("http://10.30.7.70:5000");
 export default socket;
